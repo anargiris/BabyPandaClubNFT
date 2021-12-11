e3c36870ef68a912e00b2e95fedb1311704a6bed
anargiris <anargiriss@hotmail.com>
2021-12-12 01:55:23 +0200

    gtag

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,22 @@ export default function Home() {
           property="og:image"
           content="https://babypandaclub.io/portrait.jpg"
         />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <div className="bg-bg">
         <header
